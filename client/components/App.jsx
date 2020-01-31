6e957e593716b8809b58d440e432257e5df6133b
Faith <pfaithmtan@gmail.com>
2020-01-31 14:54:53 -0800

    Init

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                App goes here!
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`App server listening on port: ${port}!`))
