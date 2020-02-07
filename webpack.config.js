d0f383135bb6277ca3ad63b63d8a971d197dd6b1
Faith <pfaithmtan@gmail.com>
2020-02-07 12:54:00 -0800

    Fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 const path = require('path');
+
 const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-    entry: `${SRC_DIR}/index.jsx`,
-    output: {
-        filename: 'bundle.js',
-        path: DIST_DIR,
-    },
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-        ],
-    },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
 
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
