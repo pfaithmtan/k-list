0e43bd14e263844b5fa2895e17d934d4f7610123
Faith <pfaithmtan@gmail.com>
2020-02-07 17:09:52 -0800

    Set up main page
    - Include buttons for sign up and log in

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {};
-    }
+    this.state = {
+      signUp: false,
+      logIn: false,
+    };
 
-    render() {
-        return (
-            <div>
-                App goes here!
-            </div>
-        );
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.persist();
+
+    const buttonName = event.target.name;
+
+    this.setState({
+      [buttonName]: true,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+
+        <button name="signUp" type="button" onClick={this.handleClick}>Sign Up</button>
+        <button name="logIn" type="button" onClick={this.handleClick}>Log In</button>
+      </div>
+    );
+  }
 }
 
 export default App;
