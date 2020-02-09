9e515b010c296fe9697c03b0ca6d9f296e24258a
Faith <pfaithmtan@gmail.com>
2020-02-08 23:15:04 -0800

    Remove bodyParser
    - Use express.json() instead
    - Require and use morgan

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
+app.use(morgan);
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.listen(port, () => console.log(`App server listening on port: ${port}!`));
