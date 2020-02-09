fa7b3c4275833eed9d9b2c8837718167e6cd4d64
Faith <pfaithmtan@gmail.com>
2020-02-09 14:54:01 -0800

    Convert class to functional component
    - Change to use state hook

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,40 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import SignUp from './SignUp';
 import Login from './Login';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+export default function App() {
+  const [signUp, setSignUp] = useState(false);
+  const [logIn, setLogIn] = useState(false);
 
-    this.state = {
-      signUp: false,
-      logIn: false,
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
+  const handleClick = (event) => {
     event.persist();
 
     const buttonName = event.target.name;
 
-    this.setState({
-      [buttonName]: true,
-    });
-  }
+    if (buttonName === 'signUp') {
+      setSignUp(true);
+    } else if (buttonName === 'logIn') {
+      setLogIn(true);
+    }
+  };
 
-  render() {
-    return (
-      <div>
-        {/* <div>name of app goes here</div> */}
-        {/* <Login /> */}
-        <SignUp />
-        {/* <button name="signUp" type="button" onClick={this.handleClick}>Sign Up</button> */}
-        {/* <button name="logIn" type="button" onClick={this.handleClick}>Log In</button> */}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={SignUp} />
+        <Route path="/:id" component={Login} />
+      </Switch>
+      {/* <div>name of app goes here</div> */}
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      {/* <button name="signUp" type="button" onClick={handleClick}>Sign Up</button> */}
+      <button name="logIn" type="button" onClick={handleClick}>Log In</button>
+    </div>
+  );
 }
-
-export default App;
