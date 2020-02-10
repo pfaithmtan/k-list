8b11aa453362554806a0e438b0cde63543fe10c3
Faith <pfaithmtan@gmail.com>
2020-02-09 19:59:58 -0800

    Remove useState
    - Use Link from React Router and assign name
    - Use Link from Material UI

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -1,33 +1,121 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import SignUp from './SignUp';
-import Login from './Login';
+import React from 'react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Faith Tan
+      </Link>
+      {' '}
+      {new Date().getFullYear()}
+      .
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
 export default function Homepage() {
-  const [signUp, setSignUp] = useState(false);
-  const [logIn, setLogIn] = useState(false);
-
-  const handleClick = (event) => {
-    event.persist();
-
-    const buttonName = event.target.name;
-
-    if (buttonName === 'signUp') {
-      setSignUp(true);
-    } else if (buttonName === 'logIn') {
-      setLogIn(true);
-    }
-  };
+  const classes = useStyles();
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Link to="/signUp">
-        <button name="signUp" type="button" onClick={handleClick}>Sign Up</button>
-      </Link>
-      <Link to="/logIn">
-        <button name="logIn" type="button" onClick={handleClick}>Log In</button>
-      </Link>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Album layout
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Home Page of App
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Something short and leading about the collection below—its contents, the creator, etc.
+              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+              entirely.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <ReactRouterLink to="/signUp">
+                    <Button variant="contained" color="primary">
+                      Sign Up
+                    </Button>
+                  </ReactRouterLink>
+                </Grid>
+                <Grid item>
+                  <ReactRouterLink to="/logIn">
+                    <Button variant="outlined" color="primary">
+                      Log In
+                    </Button>
+                  </ReactRouterLink>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        {/* <Container className={classes.cardGrid} maxWidth="md" /> */}
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </>
   );
 }
