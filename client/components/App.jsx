a4e84fa7a074125efa31b2337ead9a582411a236
Faith <pfaithmtan@gmail.com>
2020-02-09 18:15:13 -0800

    Add HomePage route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,35 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import HomePage from './HomePage';
 import SignUp from './SignUp';
 import Login from './Login';
 
 export default function App() {
-  const [signUp, setSignUp] = useState(false);
-  const [logIn, setLogIn] = useState(false);
-
-  const handleClick = (event) => {
-    event.persist();
-
-    const buttonName = event.target.name;
-
-    if (buttonName === 'signUp') {
-      setSignUp(true);
-    } else if (buttonName === 'logIn') {
-      setLogIn(true);
-    }
-  };
-
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={SignUp} />
-        <Route path="/:id" component={Login} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/signUp" component={SignUp} />
+        <Route path="/logIn" component={Login} />
       </Switch>
-      {/* <div>name of app goes here</div> */}
-      {/* <Login /> */}
-      {/* <SignUp /> */}
-      {/* <button name="signUp" type="button" onClick={handleClick}>Sign Up</button> */}
-      <button name="logIn" type="button" onClick={handleClick}>Log In</button>
     </div>
   );
 }
