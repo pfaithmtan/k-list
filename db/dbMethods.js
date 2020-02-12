8a38a70e3c223b586fd4ed52f95de77e34b6ec34
Faith <pfaithmtan@gmail.com>
2020-02-12 14:05:59 -0800

    Rename function to save user to db
    - Define createUser function before exporting

--- db/dbMethods.js
+++ db/dbMethods.js
@@ -1,25 +1,27 @@
 const db = require('./index');
 
-module.exports = {
-  postUser: (req, res) => {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-    } = req.body;
+const createUser = (req, res) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+  } = req.body;
 
-    db.User.create({
-      firstName,
-      lastName,
-      email,
-      password,
+  db.User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+  })
+    .then((data) => {
+      res.status(200).send(`Successfully saved ${data} in db!`);
     })
-      .then((data) => {
-        res.status(200).send(`Successfully saved ${data} in db!`);
-      })
-      .catch((error) => {
-        res.status(500).send(`There has been an error: ${error}`);
-      });
-  },
+    .catch((error) => {
+      res.status(500).send(`There has been an error: ${error}`);
+    });
+};
+
+module.exports = {
+  createUser,
 };
