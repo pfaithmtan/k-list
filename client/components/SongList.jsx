8fb0bd3300096b7163b59e7b1f7a165daff81304
Faith <pfaithmtan@gmail.com>
2020-02-14 13:27:15 -0800

    Add check list of songs
    - Handle toggle for songs
    - Use hard coded data for song list

--- client/components/SongList.jsx
+++ client/components/SongList.jsx
@@ -3,9 +3,22 @@ import axios from 'axios';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  form: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
       width: 200,
@@ -16,10 +29,24 @@ const useStyles = makeStyles((theme) => ({
 export default function SongList() {
   const classes = useStyles();
 
+  const [checked, setChecked] = React.useState([0]);
   const [values, setValues] = React.useState({
     title: '',
     artist: '',
   });
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
 
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
@@ -39,34 +66,61 @@ export default function SongList() {
   };
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Title"
-          variant="outlined"
-          value={values.title}
-          onChange={handleChange('title')}
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Artist"
-          variant="outlined"
-          value={values.artist}
-          onChange={handleChange('artist')}
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-          onClick={handleSubmit}
-        >
-          Add Song!
-        </Button>
-      </div>
-    </form>
+    <div>
+      <form className={classes.form} noValidate autoComplete="off">
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Title"
+            variant="outlined"
+            value={values.title}
+            onChange={handleChange('title')}
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Artist"
+            variant="outlined"
+            value={values.artist}
+            onChange={handleChange('artist')}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Add Song!
+          </Button>
+        </div>
+      </form>
+      <List className={classes.root}>
+        {[0, 1, 2, 3].map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`Song ${value + 1}`} />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="comments">
+                  <CommentIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+      </List>
+    </div>
   );
 }
