0b3ecd5ff76012438ec53c709e4622a5157ae654
Faith <pfaithmtan@gmail.com>
2020-02-14 23:30:52 -0800

    Configure local strategy

--- server/index.js
+++ server/index.js
@@ -1,42 +1,38 @@
 const path = require('path');
 const express = require('express');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const morgan = require('morgan');
 const controller = require('../db/dbMethods');
 
 const app = express();
 const port = 3000;
-// const requireAuth = passport.authenticate('jwt', { session: false });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
 
-// app.get('/', requireAuth, (req, res) => res.direct(''));
-// app.use('/users:id', )
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  (email, password, done) => {
+    controller.findUserByEmail(email)
+      .then((user) => {
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch((error) => {
+        return done(error);
+      });
+  },
+));
+
 app.post('/api/users', controller.createUser);
-
-app.post('/login',
-  passport.authenticate('local'),
-  (req, res) => {
-    // If this function gets called, authentication was successful.
-    // `req.user` contains the authenticated user.
-    res.redirect(`/users/${req.user.email}`);
-  });
-
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  successFlash: 'Welcome back!',
-  failureFlash: 'Invalid email or password.',
-}));
-
-app.get('/api/users/me',
-  passport.authenticate('basic', { session: false }),
-  (req, res) => {
-    res.json({ id: req.user.id, email: req.user.email });
-  });
 
 app.post('/api/users/songs', controller.addSongs);
 
